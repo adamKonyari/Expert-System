af0a6bcf2d217e8411e3bf63caaf188a3e52e7e3
adamKonyari <adam.konyari@gmail.com>
2019-02-12 13:58:23 +0100

    fact+rule parsers ok.

--- src/main/java/com/codecool/api/FactParser.java
+++ src/main/java/com/codecool/api/FactParser.java
@@ -1,11 +1,50 @@
 package com.codecool.api;
 
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
 public class FactParser {
 
 
     private FactRepository factRepository = new FactRepository();
+    private Nodelist nodeList;
+
+    public FactParser (String xmlPath){
+        loadXmlDoccument(xmlPath);
+        this.factRepository = new FactRepository();
+        this.nodeList = document.getElementsByTagName("Fact");
+        addFactsRepository();
+    }
+    private void addFactsRepository (){
+        for( int i = 0 ; i < nodeList.getLength(); i++){
+            Node tempNode = nodeList.item(i);
+            Element tempElement = (Element)tempNode;
+            String id = tempElement.getAttribute("id");
+            Node descriptionNode = tempElement.getElementsByTagName("Description").item(0);
+            String description = ((Element)descriptionNode).getAttribute("value");
+
+            Fact fact = new Fact(id,description);
+            setFactEvaluations(fact,tempElement);
+
+            this.factRepository.addFact(fact);
+        }
+    }
+    private setFactEvaluations(Fact fact, Element tempElement){
+        Element evaluationNode = (Element) tempElement.getElementsByTagName("Evals").item(0);
+        NodeList evaluations = evaluationNode.getElementsByTagName("Eval");
+
+        for(int i = 0; i < evaluations.getLength(); i++){
+            Element evaluation = (Element) evaluations.item(i);
+            String id = evaluation.getAttribute("id");
+            String value = evaluation.getTextContent();
+            fact.setFactValueById(id,Boolean.valueOf(value));
+        }
+    }
 
     public FactRepository getFactRepository() {
         return factRepository;
     }
+
 }
--- src/main/java/com/codecool/api/RuleParser.java
+++ src/main/java/com/codecool/api/RuleParser.java
@@ -1,6 +1,11 @@
 package com.codecool.api;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RuleParser extends XMLParser {
 
@@ -13,12 +18,48 @@ public class RuleParser extends XMLParser {
         this.nodeList = getDoc().getElementsByTagName("Rule");
     }
 
-
-
-
-
-
     public RuleRepository getRuleRepository() {
         return ruleRepository;
     }
+
+    private void addRulesToRepository() {
+        for (int i = 0; i < nodeList.getLength(); i++) {
+
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String id = element.getAttribute("id");
+                String questionString = element.getElementsByTagName("Question").item(0).getTextContent();
+                Answer answer = getAnswers(element);
+                Question question = new Question(id, questionString, answer);
+                this.ruleRepository.addQuestion(question);
+            }
+        }
+    }
+
+    private Answer getAnswers(Element element) {
+        Answer answer = new Answer();
+        NodeList answerList = element.getElementsByTagName("Selection");
+        for(int i = 0; i < answerList.getLength(); i++) {
+            Node answerNode = answerList.item(i);
+
+            if(answerNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element answerElement = (Element) answerNode;
+                Element valueNode = (Element) answerNode.getChildNodes().item(1);
+                boolean answerType = Boolean.valueOf(answerElement.getAttribute("value"));
+                Value value;
+
+                if(valueNode.getNodeName().equalsIgnoreCase("SingleValue")) {
+                    String param = valueNode.getAttribute("value");
+                    value = new SingeValue(param, answerType);
+                } else {
+                    List<String> params = Arrays.asList(valueNode.getAttribute("value").split(","));
+                    value = new MultipleValue(params, answerType);
+                }
+                answer.addValue(value);
+            }
+        }
+        return answer;
+    }
+
 }
